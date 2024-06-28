c0b2b71db48793b9a8c4c8ad547f9b7d6cd48147
halfward <haolee.arch@gmail.com>
2024-06-28 15:26:29 -0400

    20240628-Event

--- weather-widget/javascript/main.js
+++ weather-widget/javascript/main.js
@@ -1,8 +1,24 @@
-console.log("this works");
-const API_KEY = config.WEATHER_API_KEY;
+let input = document.querySelector(".zipcode");
+let btn = document.querySelector(".search-button");
 
-function getWeatherData() {
-    // fetch request goes in here :-)
-  }
-  
-  getWeatherData()
+getWeatherData = (zip) => {
+  let API_KEY = config.WEATHER_API_KEY
+  let API_ENDPOINT = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&APPID=${API_KEY}`;
+
+  fetch(API_ENDPOINT)
+    .then((response) => response.json())
+    .then((data) => {
+      // store the data in a variable of our choosing
+      let local_weather_data = data;
+      // log the data to the browser console
+      console.log(local_weather_data);
+    });
+}
+
+const getZipCode = (e) => {
+  e.preventDefault();
+  let ZIP_CODE = input.value;
+  getWeatherData(ZIP_CODE);
+};
+
+btn.addEventListener('click', getZipCode);
